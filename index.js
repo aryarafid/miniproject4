4041da415de3e63e0cec972a317791dd7afdfce8
Aryarafi D <aryamohamaddanendra@gmail.com>
2024-01-23 14:53:04 +0700

    up asset

--- index.js
+++ index.js
@@ -16,6 +16,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs'); // Set your templating engine
 
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs'); // Set your templating engine
+
 app.use("/history", historyRouter)
 // app.use("/login", login)
 // app.use("/logout", logout)
