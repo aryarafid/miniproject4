a80ff618fd67a93fa22c933e99fd48c8c9560a26
Aryarafi D <aryamohamaddanendra@gmail.com>
2024-01-23 11:34:02 +0700

    sementara done controller

--- controllers/historyController.js
+++ controllers/historyController.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const pool = require("../config");
+const moment = require("moment");
+
+// moment().format();
 
 const app = express();
 
@@ -12,8 +15,17 @@ const createHistory = (req, res) => {
     req.body;
 
   // cek selisih jam agar tidak 24jam
+  const format = "HH:mm";
+  const startWorkHour = moment(workStart, format);
+  const endWorkHour = moment(workEnd, format);
+  const differenceInHrs = endWorkHour.diff(startWorkHour, "hours");
 
+  // return res.json(moment().format("YYYY-MM-DD"));
 
+  // if work date != moment() -> return res fail
+  if (workDate != moment().format("YYYY-MM-DD")) {
+    return res.status(500).json("Hanya bisa memasukkan di tanggal yang sama");
+  }
 
   pool.query(
     "INSERT INTO history (nama_Tugas, prioritas, workStart, workEnd, description, workDate) VALUES (?, ?, ?, ?, ?, ?)",
@@ -56,6 +68,19 @@ const updateHistory = (req, res) => {
   const { Id } = req.params;
   const { nama_Tugas, prioritas, workStart, workEnd, description, workDate } =
     req.body;
+
+  const format = "HH:mm";
+  const startWorkHour = moment(workStart, format);
+  const endWorkHour = moment(workEnd, format);
+  const differenceInHrs = endWorkHour.diff(startWorkHour, "hours");
+
+  // return res.json(moment().format("YYYY-MM-DD"));
+
+  // if work date != moment() -> return res fail
+  if (workDate != moment().format("YYYY-MM-DD")) {
+    return res.status(500).json("Hanya bisa memasukkan di tanggal yang sama");
+  }
+
   pool.query(
     "UPDATE history SET nama_Tugas = ?, prioritas =?, workStart = ?, workEnd = ?, description = ?, workDate = ? WHERE Id = ?",
     [nama_Tugas, prioritas, workStart, workEnd, description, workDate, Id],
