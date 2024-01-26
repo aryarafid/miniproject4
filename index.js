df81e192cb105c9cdc532c4567df4e41e8b6cafe
Aryarafi D <aryamohamaddanendra@gmail.com>
2024-01-26 09:49:00 +0700

    terakhir ip

--- index.js
+++ index.js
@@ -269,9 +269,9 @@ app.get('/history4', (req, res) => {
 ////////////////////////////////////////////////////
 
 // Start server
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
-// app.listen(port, '0.0.0.0',() => {
-//   console.log(`Server running on 0.0.0.0:${port}`);
+// app.listen(port, () => {
+//   console.log(`Server running on port ${port}`);
 // });
+app.listen(port, '0.0.0.0',() => {
+  console.log(`Server running on 0.0.0.0:${port}`);
+});
